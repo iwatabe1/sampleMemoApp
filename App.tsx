728a866de95e1d84a85dba2c1023471db932b71c
Isseki WATABE <oneseki743@gmail.com>
2023-02-07 23:03:01 +0900

    Create MemoEditScreen and MemoCreateScreen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import { StyleSheet, View } from 'react-native';
 import React from 'react';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
-import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
 
 export default function App() {
   // 画面のコンポーネント
-  return <MemoListScreen />;
+  return <MemoCreateScreen />;
+  // return <MemoEditScreen />;
+  // return <MemoListScreen />;
   // return <MemoDetailScreen />;
 }
 
