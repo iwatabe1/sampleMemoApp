effe93c587f4c9fb592a9997149847a5abadd2e1
Isseki WATABE <oneseki743@gmail.com>
2023-02-08 22:14:09 +0900

    Create LogInScreen and SignUpScreen

--- App.tsx
+++ App.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 // import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoEditScreen from './src/screens/MemoEditScreen';
-import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import LogInScreen from './src/screens/LogInScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 
 export default function App() {
   // 画面のコンポーネント
-  return <MemoCreateScreen />;
+  return <SignUpScreen />;
+  // return <LogInScreen />;
+  // return <MemoCreateScreen />;
   // return <MemoEditScreen />;
   // return <MemoListScreen />;
   // return <MemoDetailScreen />;
